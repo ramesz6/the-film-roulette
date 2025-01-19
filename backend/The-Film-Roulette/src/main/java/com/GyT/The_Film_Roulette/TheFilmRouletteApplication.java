4a85444c1c06389c45e4f90260ee7242775e2d5b
Ramon Trekovanicz <ramesz6@me.com>
2025-01-19 18:42:34 +0100

    empty backend project

---
+++ backend/The-Film-Roulette/src/main/java/com/GyT/The_Film_Roulette/TheFilmRouletteApplication.java
@@ -0,0 +1,13 @@
+package com.GyT.The_Film_Roulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFilmRouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheFilmRouletteApplication.class, args);
+	}
+
+}
---
+++ backend/The-Film-Roulette/src/test/java/com/GyT/The_Film_Roulette/TheFilmRouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GyT.The_Film_Roulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheFilmRouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
