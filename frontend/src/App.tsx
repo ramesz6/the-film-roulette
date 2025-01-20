830fc703453a3bdc54d293f419d5f2555b7ad435
petergyenge <peter.gyenge@gmail.com>
2025-01-20 15:38:19 +0100

    add getRandomMovie function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,85 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import { Movie } from './api/getRandomMovie'
+import { loadMovies } from './api/getRandomMovie'
 
 function App() {
+  const [movieDatas, setMovieDatas] = useState<Movie | null>(null);
 
+  function generateRandomNumber(): string {
+    let result = Math.floor(Math.random() * 996) + 1;
+    return "" + result
+  }
+
+
+  const getMovie = async (id: string) => {
+    try {
+      const response = await loadMovies(id);
+      if (response.success && response.data) {
+        setMovieDatas(response.data);
+      } else if (response.status === 404) {
+        getMovie(generateRandomNumber());
+      } else {
+        console.error('Failed to fetch movie data');
+      }
+    } catch (error) {
+      console.error('Error fetching movie:', error);
+    }
+  }
+
+  useEffect(() => {
+    const randomId = generateRandomNumber();
+    getMovie(randomId);
+  }, []);
 
   return (
-<div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
-  )
+    <>
+      {movieDatas ? (
+        <>
+          <div className='flex flex-col justify-center items-center'>
+          <div className="card bg-base-100 w-96 shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movieDatas.poster_path}`}
+                alt="Movie Poster"
+                className="rounded-xl" />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">{movieDatas.original_title}</h2>
+              <p>{`${movieDatas.release_date?.split("-").join(".")}.`}</p>
+              <>
+                <div className="flex flex-row flex-wrap gap-2">
+                  {movieDatas.genres && movieDatas.genres.length > 0 ? (
+                    movieDatas.genres.map(genre => (
+                      <p key={genre.id} className="px-2 py-1 rounded">
+                        {genre.name}
+                      </p>
+                    ))
+                  ) : (
+                    <p>No genres available</p>
+                  )}
+                </div>
+              </>
+              <div className="card-actions">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => {
+                    getMovie(generateRandomNumber());
+                  }}
+                >
+                  Randomized
+                </button>
+              </div>
+            </div>
+          </div>
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
+  );
+
 }
 
 export default App
---
+++ frontend/src/api/getRandomMovie.ts
@@ -0,0 +1,107 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
+import { z } from "zod";
+import { api_key } from "../apiKey/themoviedb";
+
+const client = axios.create({
+  baseURL: "https://api.themoviedb.org",
+});
+
+const getMovie = async (id: string): Promise<AxiosResponse | null> => {
+  try {
+    const response = await client.get("/3/movie/" + id, { params: {api_key: api_key}},);
+    return response;
+  } catch (error) {
+    return (error as AxiosError).response || null;
+  }
+};
+
+const belongsToCollectionSchema = z.object({
+    id: z.number(),
+    name: z.string(),
+    poster_path: z.string().nullable(),
+    backdrop_path: z.string().nullable()
+  });
+  
+  const genreSchema = z.object({
+    id: z.number(),
+    name: z.string()
+  });
+  
+  const productionCompanySchema = z.object({
+    id: z.number(),
+    logo_path: z.string().nullable(),
+    name: z.string(),
+    origin_country: z.string()
+  });
+  
+  const productionCountrySchema = z.object({
+    iso_3166_1: z.string(),
+    name: z.string()
+  });
+  
+  const spokenLanguageSchema = z.object({
+    english_name: z.string(),
+    iso_639_1: z.string(),
+    name: z.string()
+  });
+  
+  const MovieApiSchema = z.object({
+    adult: z.boolean(),
+    backdrop_path: z.string().nullable(),
+    belongs_to_collection: belongsToCollectionSchema.nullable(),
+    budget: z.number(),
+    genres: z.array(genreSchema),
+    homepage: z.string().nullable(),
+    id: z.number(),
+    imdb_id: z.string().nullable(),
+    origin_country: z.array(z.string()),
+    original_language: z.string(),
+    original_title: z.string(),
+    overview: z.string().nullable(),
+    popularity: z.number(),
+    poster_path: z.string().nullable(),
+    production_companies: z.array(productionCompanySchema),
+    production_countries: z.array(productionCountrySchema),
+    release_date: z.string(),
+    revenue: z.number(),
+    runtime: z.number().nullable(),
+    spoken_languages: z.array(spokenLanguageSchema),
+    status: z.string(),
+    tagline: z.string().nullable(),
+    title: z.string(),
+    video: z.boolean(),
+    vote_average: z.number(),
+    vote_count: z.number()
+  });
+
+export type Movie = z.infer<typeof MovieApiSchema>;
+
+const validateMovie = (response: AxiosResponse): Movie | null => {
+  const result = MovieApiSchema.safeParse(response.data);
+  if (!result.success) {
+    console.log(result.error.issues);
+    return null;
+  }
+  return result.data;
+};
+
+type Response<Type> =
+  | {
+      data: Type;
+      status: number;
+      success: true;
+    }
+  | {
+      status: number;
+      success: false;
+    };
+
+export const loadMovies = async (id?:string): Promise<Response<Movie>> => {
+  const response = await getMovie(id!);
+  if (!response) return { success: false, status: 0 };
+  if (response.status !== 200)
+    return { success: false, status: response.status };
+  const data = validateMovie(response);
+  if (!data) return { success: false, status: response.status };
+  return { success: true, status: response.status, data };
+};
