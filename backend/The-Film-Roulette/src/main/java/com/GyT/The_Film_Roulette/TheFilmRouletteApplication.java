645059b3583c879fb31d2650089dc6eed310692d
Ramon Trekovanicz <ramesz6@me.com>
2025-02-02 19:34:45 +0100

    db fix

--- backend/The-Film-Roulette/src/main/java/com/GyT/The_Film_Roulette/TheFilmRouletteApplication.java
+++ backend/The-Film-Roulette/src/main/java/com/GyT/The_Film_Roulette/TheFilmRouletteApplication.java
@@ -2,8 +2,10 @@ package com.GyT.The_Film_Roulette;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.GyT.The_Film_Roulette.models") 
 public class TheFilmRouletteApplication {
 
 	public static void main(String[] args) {
