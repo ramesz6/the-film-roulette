16ae39b5e1ca7682e9ec40901ffcea5177e71a55
markkovari <kovarimarkofficial@gmail.com>
2025-02-03 18:55:08 +0100

    chore: fix user model

--- backend/The-Film-Roulette/src/main/java/com/GyT/The_Film_Roulette/services/UserService.java
+++ backend/The-Film-Roulette/src/main/java/com/GyT/The_Film_Roulette/services/UserService.java
@@ -1,8 +1,7 @@
 package com.GyT.The_Film_Roulette.services;
 
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UserService {
 
+    
 }
--- backend/The-Film-Roulette/src/main/java/com/GyT/The_Film_Roulette/services/UserServiceImpl.java
+++ backend/The-Film-Roulette/src/main/java/com/GyT/The_Film_Roulette/services/UserServiceImpl.java
@@ -1,18 +1,12 @@
 package com.GyT.The_Film_Roulette.services;
 
+import lombok.*;
 import org.springframework.stereotype.Service;
-
-import com.GyT.The_Film_Roulette.repositories.UserRepository;
-
-import lombok.RequiredArgsConstructor;
+import com.GyT.The_Film_Roulette.repositories.*;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-    private final UserRepository userRepository;
-
-
-    public void deleteProfileByName(String name) {
-        // userRepository.deleteByUsername(name);
-    }
+    private final UserRepo userRepo; 
+    // legyen jo    
 }
