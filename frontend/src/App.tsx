7ba4462937fd91804b3a2fce9834d30eabf2834a
Ramon Trekovanicz <ramesz6@me.com>
2025-02-07 14:41:07 +0100

    format code frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,15 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import { Movie } from './api/getRandomMovie'
-import { loadMovies } from './api/getRandomMovie'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Movie } from "./api/getRandomMovie";
+import { loadMovies } from "./api/getRandomMovie";
 
 function App() {
   const [movieDatas, setMovieDatas] = useState<Movie | null>(null);
 
   function generateRandomNumber(): string {
     let result = Math.floor(Math.random() * 996) + 1;
-    return "" + result
+    return "" + result;
   }
-
 
   const getMovie = async (id: string) => {
     try {
@@ -20,12 +19,12 @@ function App() {
       } else if (response.status === 404) {
         getMovie(generateRandomNumber());
       } else {
-        console.error('Failed to fetch movie data');
+        console.error("Failed to fetch movie data");
       }
     } catch (error) {
-      console.error('Error fetching movie:', error);
+      console.error("Error fetching movie:", error);
     }
-  }
+  };
 
   useEffect(() => {
     const randomId = generateRandomNumber();
@@ -34,47 +33,48 @@ function App() {
 
   return (
     <>
-  {movieDatas ? (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <div className="card bg-base-100 w-96 shadow-xl">
-        <figure className="px-10 pt-10">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movieDatas.poster_path}`}
-            alt="Movie Poster"
-            className="rounded-xl"
-          />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">{movieDatas.original_title}</h2>
-          <p>{`${movieDatas.release_date?.split("-").join(".")}.`}</p>
-          <div className="flex flex-row flex-wrap gap-2">
-            {movieDatas.genres && movieDatas.genres.length > 0 ? (
-              movieDatas.genres.map(genre => (
-                <p key={genre.id} className="px-2 py-1 rounded">{genre.name}</p>
-              ))
-            ) : (
-              <p>No genres available</p>
-            )}
-          </div>
-          <div className="card-actions">
-            <button
-              className="btn btn-primary"
-              onClick={() => {
-                getMovie(generateRandomNumber());
-              }}
-            >
-              Randomized
-            </button>
+      {movieDatas ? (
+        <div className="flex flex-col justify-center items-center min-h-screen">
+          <div className="card bg-base-100 w-96 shadow-xl">
+            <figure className="px-10 pt-10">
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movieDatas.poster_path}`}
+                alt="Movie Poster"
+                className="rounded-xl"
+              />
+            </figure>
+            <div className="card-body items-center text-center">
+              <h2 className="card-title">{movieDatas.original_title}</h2>
+              <p>{`${movieDatas.release_date?.split("-").join(".")}.`}</p>
+              <div className="flex flex-row flex-wrap gap-2">
+                {movieDatas.genres && movieDatas.genres.length > 0 ? (
+                  movieDatas.genres.map((genre) => (
+                    <p key={genre.id} className="px-2 py-1 rounded">
+                      {genre.name}
+                    </p>
+                  ))
+                ) : (
+                  <p>No genres available</p>
+                )}
+              </div>
+              <div className="card-actions">
+                <button
+                  className="btn btn-primary"
+                  onClick={() => {
+                    getMovie(generateRandomNumber());
+                  }}
+                >
+                  Randomized
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  ) : (
-    <p>Loading...</p>
-  )}
-</>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
   );
-
 }
 
-export default App
+export default App;
--- frontend/src/apiKey/themoviedb.ts
+++ frontend/src/apiKey/themoviedb.ts
@@ -1,1 +1,1 @@
-export let api_key = "98c7ccf7c12cac28e33c1b504e958e85"
+export let api_key = "98c7ccf7c12cac28e33c1b504e958e85";
