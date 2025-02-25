4e1ee1d6bb417715264fb4c1093e6630125b1904
Ramon Trekovanicz <ramesz6@me.com>
2025-02-25 14:28:45 +0100

    corsconfig

--- backend/The-Film-Roulette/src/main/java/com/gyt/thefilmroulette/security/ApplicationSecurity.java
+++ backend/The-Film-Roulette/src/main/java/com/gyt/thefilmroulette/security/ApplicationSecurity.java
@@ -1,5 +1,9 @@
 package com.gyt.thefilmroulette.security;
 
+import com.gyt.thefilmroulette.configurations.CorsConfig;
+import java.util.Arrays;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,6 +12,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 /**
  * Security configuration for the application, setting up authentication and
@@ -18,7 +25,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class ApplicationSecurity {
+
+  private final CorsConfig corsConfig;
 
   /**
    * A list of allowed paths that do not require authentication.
@@ -42,5 +52,26 @@ public class ApplicationSecurity {
         .httpBasic(Customizer.withDefaults())
         .formLogin(Customizer.withDefaults());
     return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.stream(corsConfig.getCorsUrls().split(",")).toList());
+    configuration
+        .setAllowedMethods(Arrays.asList(
+            "GET",
+            "POST",
+            "PUT",
+            "PATCH",
+            "DELETE",
+            "OPTIONS",
+            "HEAD"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 }
